a2292114d5063212c69f890c7d309ea46d9ffc80
Tessie <mrxy56@gmail.com>
2023-01-05 16:41:25 -0500

    Add files via upload

---
+++ 434. Number of Segments in a String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSegments(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        int segments = 0;
+        for(int i = 0; i < s.length(); i++)
+            if(s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' '))
+                segments++;
+        return segments;
+    }
+}
