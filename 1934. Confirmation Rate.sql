2f34cfc9cf5e5a8741cacc53fd4b78a2f63bf202
Tessie <mrxy56@gmail.com>
2023-09-17 14:08:00 -0400

    Add files via upload

---
+++ 1934. Confirmation Rate.sql
@@ -0,0 +1,5 @@
+# AVG(IF(c.action = 'confirmed', 1, 0)) is the key.
+SELECT s.user_id, ROUND(AVG(IF(c.action = 'confirmed', 1, 0)), 2) AS confirmation_rate
+FROM Signups s LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
