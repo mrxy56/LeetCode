c2f98d83dc8d2292ec4a4c770997e01a6dc39988
Tessie <mrxy56@gmail.com>
2023-09-17 13:50:30 -0400

    Add files via upload

---
+++ 1280. Students and Examinations.sql
@@ -0,0 +1,6 @@
+# CROSS JOIN and LEFT JOIN, count the subject_name of Examinations.
+SELECT s.student_id, s.student_name, sb.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s CROSS JOIN Subjects sb LEFT JOIN Examinations e
+ON s.student_id = e.student_id AND sb.subject_name = e.subject_name
+GROUP BY s.student_id, sb.subject_name
+ORDER BY s.student_id, sb.subject_name
